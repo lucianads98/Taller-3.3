744f8580dc7ff0da981fa9ab4efdc71615c57240
Luciana De Souza <129917797+lucianads98@users.noreply.github.com>
2024-08-15 19:42:30 -0300

    Autorizando

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -5,3 +5,16 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.getElementsByClassName("col-sm-6").addEventListener("submit", function(event) {
+    const password1 = document.getElementById("password1").value;
+    const password2 = document.getElementById("password2").value;
+    const mensajeError = document.getElementById("mensajeError");
+    
+    if (password1 !== password2) {
+        mensajeError.textContent = "Las contraseñas no coinciden.";
+        event.preventDefault();  
+    } else {
+        mensajeError.textContent = "";
+    }
+});
