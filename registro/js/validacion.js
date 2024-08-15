2de95565e89b9f6f5c6c70790a6fdcd8fb3fac43
Valentina Carrique <valentinacarriqueviera@gmail.com>
2024-08-15 20:01:20 -0300

    Cambios

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -5,3 +5,12 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.getElementById("regBtn").addEventListener("click", function() {
+    let nombre = document.getElementById("nombre").ariaValueMax.trim();
+    const apellido = document.getElementById("apellido").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password1 = document.getElementById("password1").value;
+    const password2 = document.getElementById("password2").value;
+    const terminos = document.getElementById("terminos").checked;
+})
