27e2cbea018a6bce9a01f230166b2fce950a90f9
Letimachado <leti.machado.gerez@gmail.com>
2024-08-15 21:07:04 -0300

    Ahora si funciona

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -1,5 +1,3 @@
-document.getElementById("form").addEventListener("submit", function(event) {
-     event.preventDefault();
 
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
@@ -7,42 +5,25 @@ function showAlertSuccess() {
 
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
+} 
+
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+const nombre = document.getElementById('nombre').value;
+const email = document.getElementById('email').value;
+const apellido = document.getElementById('apellido').value;
+const contraseña = document.getElementById('password1').value;
+const repcontraseña = document.getElementById('password2').value;
+const terminos = document.getElementById('terminos').checked;
+
+if(nombre === "" || email === "" || apellido === "" || contraseña.length < 6 || contraseña !== repcontraseña || !terminos) {
+    showAlertError();
+    return;
+} else {
+    showAlertSuccess();
 }
 
 
-const nombre = document.getElementById('nombre')
-const email = document.getElementById('email')
-const apellido = document.getElementById('apellido')
-const contraseña = document.getElementById('password1')
-const repcontraseña = document.getElementById('password2')
 
-if(password1 <= 6){
-showAlertError()
-}
-     
-    if (password1 !== password2) {
-        showAlertError()
-    }
-
-      if(nombre.value == 0) {
-    
-        showAlertError()
-    }else{
-        showAlertSuccess()
-    }
-
-    if(apellido.value == 0) {
-    
-        showAlertError()
-    }else{
-        showAlertSuccess()
-    }
-
-    if(email.value == 0) {
-    
-        showAlertError()
-    }else{
-        showAlertSuccess()
-    }
-}));
-
+});
